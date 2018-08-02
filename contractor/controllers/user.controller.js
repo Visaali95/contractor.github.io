d1f14a3a6574ecf1b1d1f9bce955da5ac9895f05
Visaali95 <visaali.s@parangat.com>
2018-08-02 17:51:57 +0530

    adding contractor proj

---
+++ contractor/controllers/user.controller.js
@@ -0,0 +1,123 @@
+const { User }      = require('../models');
+const authService   = require('../services/auth.service');
+const { to, ReE, ReS }  = require('../services/util.service');
+
+const create = async function(req, res){
+    res.setHeader('Content-Type', 'application/json');
+    const body = req.body;
+    if(!body.email && !body.phone && body.fbid && body.twitterid && body.pintrestid && body.instaid){
+        return ReE(res, 'Please enter an email or phone number or social media to register.');
+    } else if((body.email || body.phone) && !body.password){
+        return ReE(res, 'Please enter a password to register.');
+    }else if((body.email || body.phone) && !body.postcode){
+        return ReE(res, 'Please enter a postcode');
+    }else{
+        let err, user;
+
+        [err, user] = await to(authService.createUser(body));
+
+        if(err) return ReE(res, err, 422);
+        return ReS(res, {message:'Successfully created new user.', user:user.toWeb(), token:user.getJWT()}, 201);
+    }
+}
+module.exports.create = create;
+
+const get = async function(req, res){
+    res.setHeader('Content-Type', 'application/json');
+    let user = req.user;
+
+    return ReS(res, {user:user.toWeb()});
+}
+module.exports.get = get;
+
+const update = async function(req, res){
+    let err, user, data
+    user = req.user;
+    data = req.body;
+    user.set(data);
+
+    User.findById(user._id,function(err, user){
+        if(err) return ReE(res, err);
+        else if(user){
+            if(data.email){
+                User.findOne({email:data.email,id:{$ne:user._id}},function(err,user){
+                    if(err) return ReE(res, err);
+                    if(user) return ReE(res, "email id already exits");
+                });
+            }else if(data.fbid){
+                User.findOne({fbid:data.fbid,id:{$ne:user._id}},function(err,user){
+                    if(err) return ReE(res, err);
+                    if(user) return ReE(res, "FBid id already exits");
+                });
+            }else if(data.phone){
+                User.findOne({phone:data.phone,id:{$ne:user._id}},function(err,user){
+                    if(err) return ReE(res, err);
+                    if(user) return ReE(res, "Phone number already exits");
+                });
+            }else{
+                User.findByIdAndUpdate(user._id,{$set:req.body}, function(err, result){
+                    if(err){
+                         return ReE(res, err);
+                    }
+                   // console.log("RESULT: " + result);
+                     return ReS(res, {message:"Updated Successfully"});
+                }); 
+            }
+            
+            
+
+        }else{
+            return ReE(res, 'Invalid user token');
+        }
+        
+         
+
+    });
+    //console.log(user);
+    //err.message = 'Invalid user tokenn';
+    //return ReE(res, err);
+    //[err, user] = await to(user.save());
+        //return ReS(res, {message:user});
+    /*[err, user] = await to(user.save());
+    if(err){
+        console.log(err, user);
+
+        if(err.message.includes('E11000')){
+            if(err.message.includes('phone')){
+                err = 'This phone number is already in use';
+            } else if(err.message.includes('email')){
+                err = 'This email address is already in use';
+            }else{
+                err = 'Duplicate Key Entry';
+            }
+        }
+
+        return ReE(res, err);
+    }
+    return ReS(res, {message :'Updated User: '+user.email});*/
+}
+module.exports.update = update;
+
+const remove = async function(req, res){
+    let user, err;
+    user = req.user;
+
+    [err, user] = await to(user.destroy());
+    if(err) return ReE(res, 'error occured trying to delete user');
+
+    return ReS(res, {message:'Deleted User'}, 204);
+}
+module.exports.remove = remove;
+
+
+const login = async function(req, res){
+    const body = req.body;
+    let err, user;
+    //return ReS(res, {token:body});
+    
+    [err, user] = await to(authService.authUser(req.body));
+    if(err) return ReE(res, err, 422);
+
+    return ReS(res, {token:user.getJWT(), user:user.toWeb()});
+}
+module.exports.login = login;
---
+++ contractor/services/auth.service.js
@@ -0,0 +1,146 @@
+const { User } 	    = require('../models');
+const validator     = require('validator');
+const { to, TE }    = require('../services/util.service');
+
+const getUniqueKeyFromBody = function(body){// this is so they can send in 3 options unique_key, email, or phone and it will work
+    let unique_key = body.unique_key;
+    
+    if(typeof unique_key==='undefined'){
+        if(typeof body.fbid != 'undefined'  && body.fbid!=''){
+            unique_key = body.fbid
+       }else if(typeof body.twitterid != 'undefined'  && body.twitterid!=''){
+            unique_key = body.twitterid
+       }else if(typeof body.instaid != 'undefined'  && body.instaid!=''){
+            unique_key = body.instaid
+       }else if(typeof body.pintrestid != 'undefined'  && body.pintrestid!=''){
+            unique_key = body.pintrestid
+       }else if(typeof body.email != 'undefined'  && body.email!=''){
+            unique_key = body.email
+        }else if(typeof body.phone != 'undefined' && body.phone!=''){
+            unique_key = body.phone
+        }else{
+            unique_key = null;
+        }
+    }
+
+    return unique_key;
+}
+module.exports.getUniqueKeyFromBody = getUniqueKeyFromBody;
+
+
+const createUser = async function(userInfo){
+    let unique_key, auth_info, err;
+
+    auth_info={}
+    auth_info.status='create';
+
+    unique_key = getUniqueKeyFromBody(userInfo);
+    if(!unique_key) TE('An email or phone number or social media was not entered.');
+
+    let validationError ='';
+    if(validator.isEmail(unique_key)){
+        //console.log(userInfo);
+        auth_info.method = 'email';
+        userInfo.email = unique_key;
+        [err, user] = await to(User.findOne({email:unique_key }));
+        //console.log(JSON.stringify(user));
+        if(Boolean(user)) TE('Email id already exits');
+
+    }
+
+    if(validator.isMobilePhone(unique_key, 'any')){//checks if only phone number was sent
+        auth_info.method = 'phone';
+        userInfo.phone = unique_key;
+        [err, user] = await to(User.findOne({phone:phone }));
+        if(Boolean(user)) TE('Phone number already exits');
+        //[err, user] = await to(User.create(userInfo));
+        //if(err) TE('user already exists' +JSON.stringify(err));
+
+        //return user;
+    }
+
+    if(userInfo.fbid){
+        [err, user] = await to(User.findOne({fbid:userInfo.fbid }));
+        userInfo.password='123';
+        if(Boolean(user)) TE('Facebook id already exits');
+    }
+    if(userInfo.twitterid){
+        userInfo.password='123';
+        [err, user] = await to(User.findOne({twitterid:userInfo.twitterid }));
+        if(Boolean(user)) TE('Twitter id already exits');
+    }
+    if(userInfo.pintrestid){
+        userInfo.password='123';
+        [err, user] = await to(User.findOne({pintrestid:userInfo.pintrestid }));
+        if(Boolean(user)) TE('Pintrest id already exits');
+    }
+    if(userInfo.instaid){
+        userInfo.password='123';
+        [err, user] = await to(User.findOne({instaid:userInfo.instaid }));
+        if(Boolean(user)) TE('Pintrest id already exits');
+    }
+    //console.log(userInfo);
+    userInfo.status = true;
+    [err, user] = await to(User.create(userInfo));
+    if(err) TE('Invalid Parameter'+JSON.stringify(err));
+
+    return user;
+    
+}
+module.exports.createUser = createUser;
+
+const authUser = async function(userInfo){//returns token
+    let unique_key;
+    let auth_info = {};
+    auth_info.status = 'login';
+    unique_key = getUniqueKeyFromBody(userInfo);
+    
+
+    if(!unique_key) TE('Please enter an email or phone number or social media to login');
+
+
+    if((userInfo.email || userInfo.phone) && (!userInfo.password)) TE('Please enter a password to login');
+
+    let user,emailphone;
+    if(validator.isEmail(unique_key)){
+        auth_info.method='email';
+        emailphone =1;
+        [err, user] = await to(User.findOne({email:unique_key }));
+        if(err) TE(err.message);
+
+    }else if(validator.isMobilePhone(unique_key, 'any')){//checks if only phone number was sent
+        auth_info.method='phone';
+        emailphone =1;
+        [err, user] = await to(User.findOne({phone:unique_key }));
+        if(err) TE(err.message);
+
+    }else if(userInfo.fbid){
+        [err, user] = await to(User.findOne({fbid:userInfo.fbid }));
+        if(err) TE(err.message);
+    }else if(userInfo.twitterid){
+        [err, user] = await to(User.findOne({twitterid:userInfo.twitterid }));
+        if(err) TE(err.message);
+    }else if(userInfo.pintrestid){
+        [err, user] = await to(User.findOne({pintrestid:userInfo.pintrestid }));
+        if(err) TE(err.message);
+    }else if(userInfo.instaid){
+        [err, user] = await to(User.findOne({instaid:userInfo.instaid }));
+        if(err) TE(err.message);
+    }else{
+        TE('A valid email or phone number or social media was not entered');
+    }
+
+    if(!user) TE('Not registered');
+
+    if(emailphone){
+        [err, user] = await to(user.comparePassword(userInfo.password));
+
+        
+    }
+    if(err) TE(err.message);
+     
+
+    return user;
+
+}
+module.exports.authUser = authUser;
