d2edfbc8d9a01769d65e7761cf2e647e25f51e22
Visaali95 <visaali.s@parangat.com>
2018-08-30 17:57:36 +0530

    config updated

--- contractor/config/config.js
+++ contractor/config/config.js
@@ -6,7 +6,7 @@ CONFIG.app = process.env.APP || "development";
 CONFIG.port = process.env.PORT || "8081";
 
 CONFIG.db_dialect = process.env.DB_DIALECT || "mongo";
-CONFIG.db_host = process.env.DB_HOST || "18.222.231.171";
+CONFIG.db_host = process.env.DB_HOST || "localhost";
 CONFIG.db_port = process.env.DB_PORT || "27017";
 CONFIG.db_name = process.env.DB_NAME || "contractor";
 CONFIG.db_user = process.env.DB_USER || "root";
